52d397720d56053c23c6abfaf48eb8e4c1aabf68
Marcos <progmark@gmail.com>
2024-11-12 03:32:12 -0500

    dos

--- app/src/main/java/com/example/sistemaventas/RetrofitInstance.kt
+++ app/src/main/java/com/example/sistemaventas/RetrofitInstance.kt
@@ -4,7 +4,7 @@
     import retrofit2.converter.gson.GsonConverterFactory
 
     object RetrofitInstance {
-        private const val BASE_URL = "http://10.80.98.37:8080/"
+        private const val BASE_URL = "http://192.168.1.33:8080/"
 
         val api: ProductoApiService by lazy {
             Retrofit.Builder()
